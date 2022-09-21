a1ef9536f9b8d7738a7e0fec316b1c703cd347c7
Osamu Ito <mr.samito@gmail.com>
2022-09-21 11:11:57 -0400

    commit tree bc9f5bec729cecd7f1fb6609e710c9368f61a271

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";//use state added
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,20 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isDarkMode, setIsDarkMode] = useState(false)// the state added for dark mode
+//Then, use that variable to determine if our app should be in dark mode or light mode.
+//replaced the "false" in appClass w/isDarkMode variable to determine the class.
+  const appClass = isDarkMode ? "App dark" : "App light"
+
+  function handleClick(){
+    setIsDarkMode((isDarkMode)=> !isDarkMode)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{isDarkMode ? "Light" : "Dark"} Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isAddedToCart, setIsAddedToCart] = useState("false")
+  
+  const clickAdd = () => {
+    setIsAddedToCart((isAddedToCart) => !isAddedToCart)
+  }
+  const cartClass = isAddedToCart ? "" : "in-cart";
+
   return (
-    <li className="">
+    <li className= {cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isAddedToCart ? "add" : "remove"} onClick={clickAdd}>{isAddedToCart ? "Add to" : "Remove From"} Cart</button>
     </li>
   );
 }
